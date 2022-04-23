383b10a3d0a951f619cc6a93880fa05dcb2e76c2
Hugo Alvarez <hualvarez8@gmail.com>
2022-04-23 19:44:35 +0200

    Models Update

--- PadelReserva/models/User.model.js
+++ PadelReserva/models/User.model.js
@@ -1,16 +1,30 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true
     },
-    password: String,
+    name: {
+      type: String
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    matches: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Match'
+    }],
+    favouriteClub: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Match'
+    }]
   },
+  
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
+
     timestamps: true,
   }
 );
